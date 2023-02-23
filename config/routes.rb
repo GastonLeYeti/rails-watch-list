2c6c6055e1e7ff472ae04fb0a1c2c4deba6cb419
gaetan pecqueux <gaetan.pecqueux@gmail.com>
2023-02-23 12:29:52 +0100

    ✨ add list && bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root 'movies#index'
+  get 'movies', to: 'movies#index'
+  get 'movies/new', to: 'movies#new'
+  post 'movies', to: 'movies#create'
+  get 'movies/:id', to: 'movies#show', as: :movie
+  get 'movies/:id/edit', to: 'movies#edit', as: :edit_movie
+  patch 'movies/:id', to: 'movies#update'
+  delete 'movies/:id', to: 'movies#destroy'
+
+
 end
