514de36f2af8efa4b99c503b50a4fcf5b42f03a5
gaetan pecqueux <gaetan.pecqueux@gmail.com>
2023-02-23 11:16:42 +0100

    ✨ end init projet

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,10 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
+
+  config.sass.inline_source_maps = true
+
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
